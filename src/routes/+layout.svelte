b54c17e59215802b2ad113ecf383c1c911052f01
Michael Tran <michaeltranperson@gmail.com>
2023-05-18 01:00:40 -0600

    changing it up for mac!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script>
 	import '../app.css';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+  let isMac = false;
+  let isWindows = false;
+
+  onMount(() => {
+    const body = document.querySelector('body');
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.indexOf('Mac') !== -1) {
+      isMac = true;
+    } else if (userAgent.indexOf('Windows') !== -1) {
+      isWindows = true;
+    }
+  });
   //import PageTransition from '../component/PageTransition.svelte';
 
   //class:bg-macsilver={$page.url.pathname === '/portfolio'} class:bg-lightmacintosh={$page.url.pathname === '/about'}
@@ -40,10 +55,15 @@
 			class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400"
 			href="/"
 			>
-			<span tooltip="Mikesenh" flow="right">
+			{#if isWindows}
+			<span tooltip="Mikesenh Windows" flow="right">
 				<img on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} on:blur={onBlur} on:focus={onFocus}  class="h-14 lg:h-24 hover:scale-105 transition" src={colour} alt="">
-				
 				</span>
+				{:else if isMac}
+				<span mac-tooltip="Mikesenh Macintosh" flow="right">
+					<img on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} on:blur={onBlur} on:focus={onFocus}  class="h-14 lg:h-24 hover:scale-105 transition" src={colour} alt="">
+					</span>
+			{/if}	
 		  </a>
 		  <!-- Mobile menu button -->
 		  <button on:click={toggleNavbar} class="flex md:hidden">
@@ -91,11 +111,17 @@
 	class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400"
 	href="/"
 	>
+	{#if isWindows}
 	<span tooltip="Mikesenh" flow="right">
 		<img on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} on:blur={onBlur} on:focus={onFocus}  class="mb-3
 		 h-14 lg:h-24 hover:scale-105 transition" src={colour} alt="">
-		
 		</span>
+	{:else if isMac}
+	<span mac-tooltip="Mikesenh" flow="right">
+		<img on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} on:blur={onBlur} on:focus={onFocus}  class="mb-3
+		 h-14 lg:h-24 hover:scale-105 transition" src={colour} alt="">
+		</span>
+	{/if}
   </a>
 </div>
 </div>
@@ -332,6 +358,100 @@
 
 [tooltip][flow^="right"]:hover::before,
 [tooltip][flow^="right"]:hover::after {
+  animation: tooltips-horz 300ms ease-out forwards;
+}
+
+/*Mac*/
+
+  /* START TOOLTIP STYLES */
+  [mac-tooltip] {
+  position: relative; /* opinion 1 */
+}
+
+/* Applies to all tooltips */
+[mac-tooltip]::before,
+[mac-tooltip]::after {
+  text-transform: none; /* opinion 2 */
+  font-size: .9em; /* opinion 3 */
+  line-height: 1;
+  user-select: none;
+  pointer-events: none;
+  position: absolute;
+  display: none;
+  opacity: 0;
+}
+[mac-tooltip]::before {
+  content: '';
+  border: 5px solid transparent; /* opinion 4 */
+  z-index: 1001; /* absurdity 1 */
+}
+[mac-tooltip]::after {
+  content: attr(mac-tooltip); /* magic! */
+  
+  /* most of the rest of this is opinion */
+  font-family: 'Jost', sans-serif;
+  text-align: center;
+  
+  /* 
+    Let the content set the size of the tooltips 
+    but this will also keep them from being obnoxious
+    */
+  min-width: 3em;
+  max-width: 21em;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 1ch 1.5ch;
+  border-radius: .3ch;
+  box-shadow: 0 1em 2em -.5em rgba(0, 0, 0, 0.35);
+  background: #333;
+  color: #fff;
+  z-index: 1000; /* absurdity 2 */
+}
+
+/* Make the tooltips respond to hover */
+[mac-tooltip]:hover::before,
+[mac-tooltip]:hover::after {
+  display: block;
+}
+
+/* don't show empty tooltips */
+[mac-tooltip='']::before,
+[mac-tooltip='']::after {
+  display: none !important;
+}
+
+/* FLOW: RIGHT */
+[mac-tooltip][flow^="right"]::before {
+  top: 65%;
+  border-left-width: 0;
+  border-right-color: #333;
+  right: calc(0em - 15px);
+  transform: translate(.5em, -50%);
+}
+[mac-tooltip][flow^="right"]::after {
+  top: 65%;
+  left: calc(100% + 15px);
+  transform: translate(.5em, -50%);
+}
+
+/* KEYFRAMES */
+@keyframes mac-tooltips-vert {
+  to {
+    opacity: .9;
+    transform: translate(-50%, 0);
+  }
+}
+
+@keyframes mac-tooltips-horz {
+  to {
+    opacity: .9;
+    transform: translate(0, -50%);
+  }
+}
+
+[mac-tooltip][flow^="right"]:hover::before,
+[mac-tooltip][flow^="right"]:hover::after {
   animation: tooltips-horz 300ms ease-out forwards;
 }
 </style>
