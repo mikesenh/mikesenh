79ec9714d3b22ff6c07d366a1f85d6f12bdce4ce
Michael Tran <michaeltranperson@gmail.com>
2023-06-22 22:33:57 -0600

    a bunch of edits

---
+++ src/routes/work/project-iit/+page.svelte
@@ -0,0 +1,116 @@
+<div class="text-center my-12 mb-24 mx-5">
+  <p class="text-lg mb-3">Design Work</p>
+  <h1 class="lg:text-6xl text-5xl font-semibold">Incredibly Interconnectable Toys</h1>
+  <p class="text-xl mt-3">Unleash The Curiosity</p>
+</div>
+
+<div>
+  <div
+    class="flex 2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 md:flex-row flex-col bg-white rounded-xl p-10 shadow-2xl"
+  >
+    <img class="w-1/6 self-start" src="/base/move-macintosh.webp" alt="" />
+    <span class="self-start mt-5 mx-2">
+      <h2 class="text-2xl font-semibold">Objective</h2>
+      <p class="text-lg">
+        In this project, I embarked on a conceptual design of the website for Incredibly Interconnectable Toys, showcasing my expertise as a web designer. My aim was to create a playful and engaging website that caters to children while maintaining a level of sophistication for adult users. This project was particularly enjoyable for me, as it provided a unique opportunity to embrace the whimsical aspect of design that I don't often have the chance to explore.
+      </p>
+    </span>
+    <span class="self-start mt-5 pr-10 mx-3">
+      <h2 class="text-2xl font-semibold">Services</h2>
+      <p class="text-lg ">
+        Web&nbsp;Design <br />
+        UI&nbsp;Design  <br />
+      </p>
+    </span>
+  </div>
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+    <div class="border-b-4 border-myblue pb-3">
+        <h3 class="text-3xl font-semibold">
+          Making a Cohesive and Engaging Signature Design
+        </h3>
+      </div>
+      <p class="mb-12 mt-6 text-lg">
+        When designing the toy website, my main focus was to create a visual representation that would resonate with both parents and children. To achieve this, I incorporated playful curves as the very first concept. I believed that these wavy lines would evoke a smooth and innocent feel, inviting visitors to explore the website. One of my goals was to establish a repeating theme throughout the website, making it iconic and memorable. While this concept is yet to be tested, the initial rendering of the design felt good and aligned with my primary objective. I aimed to create a system where each page would have its own unique touch while still being part of the same cohesive family.
+      </p>
+  <img
+    class="rounded-xl shadow-2xl w-full"
+    loading="lazy"
+    src="/project/toys/toys-curve.webp"
+    alt=""
+  />
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+  <div class="border-b-4 border-myblue pb-3">
+    <h3 class="text-3xl font-semibold">
+      Showcasing the Infinite Potential of Incredibly Interconnectable Toys
+    </h3>
+  </div>
+  <p class="mb-12 mt-6 text-lg">
+    The goal of the website was not just to present the toys as mere playthings, but to demonstrate that they were meant to be anything the user desired. By highlighting the versatility and potential for creative expression, I sought to convey the message that being creative is about seeing the possibilities and feeling inspired.  I wanted the visuals of the toys to serve as a practical guide for instructions, enabling users to easily understand the various ways in which the toys could be connected. By showcasing the toys in diverse shapes and configurations, I aimed to spark the imagination of visitors and encourage them to explore the endless possibilities.
+
+
+  </p>
+  <img
+    class="rounded-xl shadow-lg w-full"
+    loading="lazy"
+    src="/project/toys/toys-display.webp"
+    alt=""
+  />
+  <p class="my-10 text-lg">
+    
+
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+  <div class="border-b-4 border-myblue pb-3">
+    <h3 class="text-3xl font-semibold">
+      Creating an Effortless Checkout Experience
+    </h3>
+  </div>
+  <p class="mb-12 mt-6 text-lg">
+    One of the key objectives in designing the toy website was to optimize the checkout process, aiming to minimize any potential difficulties for customers. To accomplish this, I dedicated considerable effort to conducting thorough research, particularly by studying the checkout procedures implemented by other successful toy brands. This research allowed me to gather valuable insights and identify patterns that were crucial to parents during the checkout phase. 
+
+
+  </p>
+  <img
+    class="rounded-xl shadow-lg w-full"
+    loading="lazy"
+    src="/project/toys/toys-cart.webp"
+    alt=""
+  />
+  <p class="my-10 text-lg">
+    
+
+</div>
+
+  <div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-10 select-none">
+    
+
+  <img
+    class="rounded-xl shadow-lg w-full mt-5"
+    loading="lazy"
+    src="/project/toys/toys-takeaway.webp"
+    alt=""
+  />
+</div>
+
+<div>
+  <div
+    class="flex justify-center 2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 md:flex-row flex-col bg-white rounded-xl p-10 shadow-2xl mb-20"
+  >
+   
+    <span class="self-center mt-5 mx-2 xl:w-3/5">
+      <div class="border-b-4 border-myblue pb-3">
+        <h3 class="text-3xl font-semibold">
+          Takeaway
+        </h3>
+       
+      </div>
+      <p class="my-5">Ultimately, the main takeaway from this project is that designing with intention and purpose is key. A playful UI should not only look aesthetically pleasing but also serve a meaningful purpose. By combining visual appeal with a thoughtful user experience, we can create a harmonious and captivating design that leaves a lasting impact on users and encourages them to engage with the products, brand and company.</p>
+    </span>
+    
+  </div>
+</div>
