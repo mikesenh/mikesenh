e7631c309957edb07715a4f681978ac0755e8269
mikesenh <michaeltranperson@gmail.com>
2024-02-21 03:55:22 -0700

    this is a blind test run.

---
+++ src/routes/study/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import DisplayProjectSlide from "../../components/DisplayProjectSlide.svelte";
+  import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+  
+</script>
+<Splide aria-label="My Favorite Images">
+  <SplideSlide>
+    <DisplayProjectSlide
+    img_src="/project/autosew/thumbnail.webp"
+    img_alt="project: The Grace Company work: UX, UI & Web Development"
+    extra_string="iOS / UX Design / UX Research/ UI Design"
+    title_string="AutoSew Software"
+    desc_string="In this project, I'd designed the UI of a quilting/sewing software. I was given the chance to conduct user research, delve into UX design, and ultimately visualize what the software could become."
+    link_href="/work/project-autosew"
+  />
+      
+  </SplideSlide>
+  <SplideSlide>
+    <DisplayProjectSlide
+    img_src="/project/autosew/thumbnail.webp"
+    img_alt="project: The Grace Company work: UX, UI & Web Development"
+    extra_string="iOS / UX Design / UX Research/ UI Design"
+    title_string="AutoSew Software"
+    desc_string="In this project, I'd designed the UI of a quilting/sewing software. I was given the chance to conduct user research, delve into UX design, and ultimately visualize what the software could become."
+    link_href="/work/project-autosew"
+  />
+      
+  </SplideSlide>
+</Splide>
+        
+        
+
+<style>
+
+</style>
