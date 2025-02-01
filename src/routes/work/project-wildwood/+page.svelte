63da5816481916e6e1bbd227d240c1262bdd81f6
mikesenh <michaeltranperson@gmail.com>
2025-01-31 22:54:21 -0700

    Final Update, Now I Can Work On Other Stuff To Update My Portfolio

---
+++ src/routes/work/project-wildwood/+page.svelte
@@ -0,0 +1,116 @@
+<div class="text-center my-12 mb-24 mx-5">
+  <p class="text-lg mb-3">Illustration & Graphic Design</p>
+  <h1 class="lg:text-6xl text-5xl font-semibold">Wildwood Mandalas Coloring Book</h1>
+  <p class="text-xl mt-3">A Coloring Journey Through Natural Patterns!</p>
+</div>
+
+<div>
+  <div
+    class="flex 2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 md:flex-row flex-col bg-white rounded-xl p-10 shadow-2xl"
+  >
+    <img class="w-1/6 self-start" src="/base/move-macintosh.webp" alt="" />
+    <span class="self-start mt-5 mx-2">
+      <h2 class="text-2xl font-semibold">Objective</h2>
+      <p class="text-lg">
+        For this project, I created Wildwood Mandalas, a coloring book designed to push my illustration skills to a more professional level. The goal was to challenge myself, explore my creative potential, and produce a polished piece that showcases my abilities. Over the course of three months, I immersed myself in the world of digital illustration, learning new techniques and refining my craft. This project not only allowed me to grow as an artist but also proved to myself that I can bring ambitious ideas to life.
+      </p>
+    </span>
+    <span class="self-start mt-5 pr-10 mx-3">
+      <h2 class="text-2xl font-semibold">Services</h2>
+      <p class="text-lg">
+        Illustration<br />
+        Graphic Design<br />
+        Branding<br />
+      </p>
+    </span>
+  </div>
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 mt-10">
+  <div class="grid lg:grid-cols-2 grid-cols-1">
+    <span>
+      <img
+        class="rounded-xl shadow-lg w-full"
+        loading="lazy"
+        src="/project/wildwood/wildwood_01.webp"
+        alt=""
+      />
+      <p class="mt-4">
+        This image presents the final cover design (+spiral-bound edition).
+      </p>
+    </span>
+    <span class="flex items-center">
+      <span class="lg:m-10 m-0">
+        <div class="border-b-4 border-myblue pb-3">
+          <h3 class="text-3xl font-semibold mt-5">
+            A Blend of Passion and Market-Ready Design
+          </h3>
+        </div>
+        <p class="mb-5 mt-6 text-lg">
+          The core idea behind this project was to create something I truly love, while also designing a product that could realistically sit on store shelves. To achieve this, I studied popular coloring books, drawing inspiration from their successful elements while infusing my own unique style to give it a personal touch. <br> <br> Every detail was carefully considered—from the professional-looking logo to the custom illustrations on the cover, and even the spine design. I thought deeply about the book’s physical form and how it could stand out, ultimately aligning my choices with what has worked for best-selling coloring book authors. This balance of research and creativity allowed me to craft something both authentic and market-ready.
+        </p>
+      </span>
+    </span>
+  </div>
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+  <div class="border-b-4 border-myblue pb-3">
+    <h3 class="text-3xl font-semibold">
+      The Art Behind Wildwood Mandalas
+    </h3>
+  </div>
+  <p class="mb-12 mt-6 text-lg">
+    To give each page its own unique identity, I drew inspiration from nature, incorporating organic elements and fine details to create cohesive themes. This project taught me that hand-drawing to perfection is a labor of love—it requires patience, consistency, and a commitment to refining your craft. Looking back, I can confidently say that Wildwood Mandalas represents a significant leap in skill and artistry compared to my previous coloring book from last year.
+  </p>
+  <img
+    class="rounded-xl shadow-2xl w-full"
+    loading="lazy"
+    src="/project/wildwood/wildwood_02.webp"
+    alt=""
+  />
+</div>
+
+
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+  <div class="border-b-4 border-myblue pb-3">
+    <h3 class="text-3xl font-semibold">
+      Reflections on Growth and Potential
+    </h3>
+  </div>
+  <p class="mb-12 mt-6 text-lg">
+    This project served as both a creative exercise and a test of my artistic capabilities. It was an opportunity to push my drawing skills to new heights and explore what I could achieve with dedication and focus. Through this process, I discovered that I’m capable of creating high-quality illustrations for any task, and it reinforced the importance of patience and effort in bringing a vision to life. I intentionally kept the design dimensions simple—working within a square format—to ensure flexibility for scaling the book to various sizes. This approach also allowed the illustrations to be easily vectorized, maintaining their quality and adaptability for future use. While this project began as a personal challenge, I believe that with a bit more refinement, Wildwood Mandalas has the potential to be a market-ready product. For now, it stands as a testament to my growth as an artist and a reminder that every project, no matter how ambitious, is built step by step.
+  </p>
+  <img
+    class="rounded-xl shadow-2xl w-full"
+    loading="lazy"
+    src="/project/wildwood/wildwood_03.webp"
+    alt=""
+  />
+</div>
+
+<div class="2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 my-20">
+ 
+  <img
+    class="rounded-xl shadow-2xl w-full"
+    loading="lazy"
+    src="/project/wildwood/wildwood_takeaway.webp"
+    alt=""
+  />
+</div>
+
+<div>
+  <div
+    class="flex justify-center 2xl:mx-40 xl:mx-20 lg:mx-10 mx-5 md:flex-row flex-col bg-white rounded-xl p-10 shadow-2xl mb-20"
+  >
+    <span class="self-center mt-5 mx-2 xl:w-3/5">
+      <div class="border-b-4 border-myblue pb-3">
+        <h3 class="text-3xl font-semibold">Takeaway</h3>
+      </div>
+      <p class="my-5">
+        This project taught me to trust my skills and embrace confidence in my abilities. I realized that striving for perfection can sometimes hold me back, and that letting go of that pressure allows me to grow and create more freely. Drawing has always been a passion of mine, and Wildwood Mandalas became a powerful way to express that love while showcasing my ability to tackle detailed, intricate work. <br> <br> Through this process, I proved to myself that I can take on projects requiring precision, custom patterns, and a high level of detail—and see them through to completion. The results speak for themselves, and that was ultimately the core objective of this project: to demonstrate what I’m capable of creating with my own hands. Moving forward, I’ll carry this confidence into future endeavors, remembering that growth comes from doing, not from waiting for things to be perfect.
+      </p>
+    </span>
+  </div>
+</div>
